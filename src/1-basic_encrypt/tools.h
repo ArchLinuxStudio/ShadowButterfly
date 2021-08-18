3ae183fca4bb4af86522950799d6c6a645e43317
TakoTakoo <85894051+TakoTakoo@users.noreply.github.com>
2021-08-18 21:16:27 +0800

    add comment for tools.h

--- src/1-basic_encrypt/tools.h
+++ src/1-basic_encrypt/tools.h
@@ -1,15 +1,75 @@
 #include "tools.c"
 
-
+/**
+ * \brief               This function sets a random number of a specified length.
+ *
+ * \param length        The length of the random number required, in bytes.
+ * \param random_num    random number to use. In \p length bytes.
+ *
+ * \return              \c 0 on success.
+ * \return              #MBEDTLS_ERR_CTR_DRBG_ENTROPY_SOURCE_FAILED or
+ *                      #MBEDTLS_ERR_CTR_DRBG_REQUEST_TOO_BIG on failure.
+ */
 int ctr_drbg_random(int length, unsigned char *random_num);
 
+/**
+ * \brief               Receive data and related parameters for AEAD encryption.
+ *                      mbedtls_cipher_context_t must be initialized.
+ *
+ * \param key           Symmetric encryption key.
+ * \param input         Data to be encrypted.
+ * \param iv            IV or Nonce to be used in AEAD.
+ * \param add           Additional Data to be used in AEAD.
+ * \param tag           The generated verification code, also called MAC or TAG.
+ * \param ret_cipher    Generated ciphertext.
+ * \param length        \p ret_cipher length.
+ * \param ctx           The generic cipher context. This must be initialized and
+ *                      bound to a key associated with an AEAD algorithm.
+ *
+ * \return              \c 0 on success.
+ * \return              #MBEDTLS_ERR_CIPHER_BAD_INPUT_DATA on
+ *                      parameter-verification failure.
+ * \return              A cipher-specific error code on failure.
+ */
 int encrypt_aes_gcm(char *key, char *input, unsigned char *iv,
                     unsigned char *add, unsigned char *tag,
                     unsigned char *ret_cipher, int *length,
                     mbedtls_cipher_context_t *ctx);
 
+
+/**
+ * \brief               Receive cipher and related parameters for AEAD decryption.
+ *                      mbedtls_cipher_context_t must be initialized.
+ *
+ * \param key           Symmetric encryption key.
+ * \param input         Data to be decrypted.
+ * \param input_length  \p input cipher length.
+ * \param iv            IV or Nonce to be used in AEAD.
+ * \param add           Additional Data to be used in AEAD.
+ * \param tag           The generated verification code, also called MAC or TAG.
+ * \param result        Generated plaintext.
+ * \param ctx           The generic cipher context. This must be initialized and
+ *                      bound to a key associated with an AEAD algorithm.
+ *
+ * \return              \c 0 on success.
+ * \return              #MBEDTLS_ERR_CIPHER_BAD_INPUT_DATA on
+ *                      parameter-verification failure.
+ * \return              #MBEDTLS_ERR_CIPHER_AUTH_FAILED if data is not authentic.
+ * \return              A cipher-specific error code on failure.
+ */
 int decrypt_aes_gcm(char *key, unsigned char *input, int input_length,
                     unsigned char *iv, unsigned char *add, unsigned char *tag,
                     unsigned char *result, mbedtls_cipher_context_t *ctx);
 
+
+/**
+ * \brief               This function outputs the data in a format that is 
+ *                      convenient for humans to read, and can output a line of prompt 
+ *                      information at the same time.
+ *
+ * \param info          The length of the random number required, in bytes.
+ * \param buf           input buffer.
+ * \param len           \p buf length.
+ * \return              NONE
+ */
 void dump_buf(char *info, uint8_t *buf, uint32_t len);
